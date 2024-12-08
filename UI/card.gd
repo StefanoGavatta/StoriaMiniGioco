c2e0ba6cb4e5c223c38f2ee04da53d37ad2ffc2d
StefanoGavatta <144794088+StefanoGavatta@users.noreply.github.com>
2024-12-08 18:36:54 +0100

    aggiunte carte con animazione

--- UI/card.gd
+++ UI/card.gd
@@ -1,6 +1,13 @@
-extends TextureRect
+extends Control
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+@export var image: Texture
+@export var costo: int
+
+
+func _ready() -> void:
+	$TextureRect.texture = image
+	
 
 
 func _mouse_entered() -> void:
