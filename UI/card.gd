1fe0aaefdc669eeec70c8aed26d0476450a27136
Stefano <stefano.gavatta@marconirovereto.it>
2024-12-20 10:10:00 +0100

    UI carte

--- UI/card.gd
+++ UI/card.gd
@@ -5,14 +5,21 @@ class_name Card
 
 @export var image: Texture
 @export var tipoCarta: String
+
 @export var costo: int
 
-@onready var GestoreGiocatore:Node = get_parent().get_parent().get_parent().get_node("%GestoreGiocatore")
+@export var vita: int
+@export var attacco: int
+@export var speed: int
 
+@onready var GestoreGiocatore:Node = get_parent().get_parent().get_parent().get_node("%GestoreGiocatore")
 func _ready() -> void:
 	#$TextureRect.texture = image  #Riaggiungere una volta terminato
-	$TextureRect/Costo.text = str(costo)
-
+	$TextureRect/Label/Costo.text = str(costo)
+	$TextureRect/Label/Vita.text = str(vita)
+	$TextureRect/Label/Attacco.text = str(attacco-2)+"~"+str(attacco+2)
+	$"TextureRect/Label/Velocità".text = str(speed)
+	$TextureRect/Label/Immagini/image.texture = image
 func _on_buy_mouse_entered() -> void:
 	if !animation_player.is_playing():
 		animation_player.play("ingrandimento")
